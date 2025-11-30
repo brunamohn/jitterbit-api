1337a45a6c8e0056b1e3162eb8d47bbdf75ac56e
brunamohn <brunamohn@gmail.com>
2025-11-30 11:33:56 -0300

    Criando tabelas

---
+++ source/db.js
@@ -0,0 +1,43 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function initDB() {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || "",
+    database: process.env.DB_NAME || "jitterbit_db"
+  });
+
+  console.log("✅ Conectado ao MySQL!");
+
+   // Criação da tabela Order
+  await connection.execute(`
+    CREATE TABLE IF NOT EXISTS \`Order\` (
+      orderId VARCHAR(50) PRIMARY KEY,
+      value DECIMAL(10,2),
+      creationDate DATETIME
+    );
+  `);
+
+  // Criação da tabela Items
+  await connection.execute(`
+    CREATE TABLE IF NOT EXISTS Items (
+      orderId VARCHAR(50),
+      productId INT,
+      quantity INT,
+      price DECIMAL(10,2),
+      FOREIGN KEY (orderId) REFERENCES \`Order\`(orderId)
+    );
+  `);
+
+  console.log("✅ Tabelas criadas com sucesso!");
+
+  await connection.end();
+}
+
+initDB().catch((err) => {
+  console.error("❌ Erro ao inicializar banco:", err);
+});
